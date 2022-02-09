6f9d449cf036d1bba6cd97dc6a821183c7ad1151
it21324888 <99323381+it21324888@users.noreply.github.com>
2022-02-09 15:06:29 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+   int marks=1, x;
+   float average;
+   while(x<2){
+      printf("mark%d = %d",x,marks);
+      scanf("%d",&marks);
+      marks=marks+1;
+      x=x+1;
+   }
+   average=marks/2;
+   printf("average = %.2f",average);
   
   return 0;
 }
